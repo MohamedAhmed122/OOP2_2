c6cdfd01d74513897fb42a6e8d2525fd67f70942
mohamed <mohamed.ahm9833@gmail.com>
2020-11-01 20:18:52 +0700

    Project is Half away

---
+++ Project2_Base/BankLibrary/Models/DemandAccount.cs
@@ -0,0 +1,18 @@
+﻿using BankLibrary.Interfaces;
+using BankLibrary.Services;
+
+namespace BankLibrary.Models
+{
+	public class DemandAccount : Account
+	{
+		public DemandAccount(decimal sum, int percentage, IEventNotifier notifier) 
+            : base(sum, percentage, notifier)
+		{
+		}
+
+		protected internal override void Open()
+		{
+            Notifier.NotifyAboutEventByType(EventType.AccountOpen, $"Opened new classic account! Id: {this.id}");
+        }
+	}
+}
---
+++ Project2_Base/BankLibrary/Models/DepositAccount.cs
@@ -0,0 +1,38 @@
+﻿using BankLibrary.Interfaces;
+
+namespace BankLibrary.Models
+{
+	public class DepositAccount : Account
+	{
+		public DepositAccount(decimal sum, int percentage, IEventNotifier notifier) : base(sum, percentage, notifier)
+		{
+		}
+		protected internal override void Open()
+		{
+			Notifier.NotifyAboutEventByType(EventType.AccountOpen,$"Opened new deposite account! Id: {this.id}");
+        }
+
+		public override void Put(decimal sum)
+		{
+			if (Days % 30 == 0)
+				base.Put(sum);
+			else
+			    Notifier.NotifyAboutEventByType(EventType.MoneyPut, "You can add money only after 30 days since account was opened!");
+        }
+
+		public override decimal Withdraw(decimal sum)
+		{
+			if (Days % 30 == 0)
+				return base.Withdraw(sum);
+
+            Notifier.NotifyAboutEventByType(EventType.Withdraw, "You can withdraw money only after 30 days since account was opened!");
+            return 0;
+		}
+
+		protected internal override void Calculate()
+		{
+			if (Days % 30 == 0)
+				base.Calculate();
+		}
+	}
+}
